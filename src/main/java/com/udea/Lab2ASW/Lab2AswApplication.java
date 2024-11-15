bf1c0c0f1f273544ba462d03cfa0969146f209ea
Santiago Hernández <sudburysoda@gmail.com>
2024-11-15 15:19:44 -0500

    Initial commit on mi-rama

---
+++ src/main/java/com/udea/Lab2ASW/Lab2AswApplication.java
@@ -0,0 +1,13 @@
+package com.udea.Lab2ASW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2AswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2AswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/Lab2ASW/Lab2AswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.Lab2ASW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2AswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
